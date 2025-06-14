33c7bf69c60153e792baf5f9eaa93b29db289529
Меньших Эдуард Максимович <emenshikh@systematica-consulting.ru>
2025-06-14 13:32:02 +0300

    added v2, v3 migrations

--- Readme.md
+++ Readme.md
@@ -3,6 +3,8 @@ psql -U user -h localhost -d postgres
 CREATE DATABASE store;
 
 ### Добавление пользователся для миграций
+psql -U user -h localhost -d store
+
 CREATE ROLE migration_account WITH LOGIN PASSWORD 'password2';
 
 GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO migration_account;
---
+++ migrations/V002__change_schema.sql
@@ -0,0 +1,17 @@
+ALTER TABLE productAdd commentMore actions
+    ADD COLUMN price DOUBLE PRECISION;
+
+ALTER TABLE orders
+    ADD COLUMN date_created DATE DEFAULT CURRENT_DATE;
+
+ALTER TABLE order_product
+    ADD CONSTRAINT fk_product FOREIGN KEY (product_id)
+        REFERENCES product (id);
+
+ALTER TABLE order_product
+    ADD CONSTRAINT fk_orders FOREIGN KEY (order_id)
+        REFERENCES orders (id);
+
+DROP TABLE orders_date;
+
+DROP TABLE product_info;
